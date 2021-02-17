a370d94cd3b5c1bfd27334f4ec3e42c089cd5031
Revanh13 <ivan@kakashi-hatake.ru>
2021-02-17 18:37:31 +0500

    Working size and reset pos and size

---
+++ Assets/Scripts/ObjectRestorer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRestorer : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    private Vector3 startPos;
+    private Vector3 startSize;
+    private Quaternion startRot;
+
+    private bool restore = false;
+    private bool restoreCoroutine = false;
+
+    public int countPickUp = 0;
+    
+    void Start()
+    {
+        startPos = GetComponent<Transform>().position;
+        startSize = GetComponent<Transform>().localScale;
+        startRot = GetComponent<Transform>().rotation;
+    }
+
+    private void Update()
+    {
+        if (countPickUp == 0) restore = true;
+
+        if (restore)
+        {
+            if (!restoreCoroutine)
+            {
+                StartCoroutine(Restore());
+                restoreCoroutine = true;
+            }
+            transform.localScale = Vector3.Lerp(transform.localScale, startSize, Time.deltaTime * speed);
+            transform.position = Vector3.Lerp(transform.position, startPos, Time.deltaTime * speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, startRot, Time.deltaTime* speed);
+        }
+    }
+
+    public void Attach()
+    {
+        countPickUp++;
+    }
+
+    public void Detach()
+    {
+        countPickUp--;
+    }
+
+    private IEnumerator Restore()
+    {
+        yield return new WaitForSeconds(1f);
+        restore = false;
+        restoreCoroutine = false;
+        print("AAAAAAAAAAAAAAAA");
+    }
+
+    // private void OnCollisionExit(Collision other)
+    // {
+    //     if (other.collider.gameObject.CompareTag("Player"))
+    //     {
+    //         restore = true;
+    //     }
+    // }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Player")) countPickUp++;
+    //
+    // }
+    //
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         countPickUp--;
+    //         if (countPickUp == 0)
+    //         {
+    //             restore = true;
+    //         }
+    //     }
+    // }
+}
